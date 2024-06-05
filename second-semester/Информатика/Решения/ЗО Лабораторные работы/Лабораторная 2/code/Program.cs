6714fc7e3219808c576a6cf0a546a59f7cc469a0
Rephobia <26533256+Rephobia@users.noreply.github.com>
2024-06-05 23:31:02 +0300

    Add laba 2 to informatics

---
+++ second-semester/Информатика/Решения/ЗО Лабораторные работы/Лабораторная 2/code/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число x");
+        int x = int.Parse(Console.ReadLine());
+        double y;
+        int branchNumber;
+        if (x < 0) {
+            y = x + Math.Sin(x);
+            branchNumber = 1;
+        } else if (x > 0) {
+            y = 5 + Math.Sqrt(Math.Pow(x, 5));
+            branchNumber = 2;
+        } else {
+            y = Math.Pow(Math.E, x);
+            branchNumber = 3;
+        }
+        Console.WriteLine("Номер ветви: " + branchNumber);
+        Console.WriteLine("Результат вычислений: " + y);
+        Console.WriteLine("Нажмите любую клавишу для выхода");
+        Console.ReadKey();
+    }
+}

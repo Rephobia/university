2870b84fbc59ce4de3f979dc913c69eba34daf42
Rephobia <26533256+Rephobia@users.noreply.github.com>
2024-06-08 22:06:56 +0300

    Add laba 4

---
+++ second-semester/Информатика/Решения/ЗО Лабораторные работы/Лабораторная 4/cod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите значение x");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите значение n");
+
+        int n = Int32.Parse(Console.ReadLine());
+        double r = 1;
+
+        for (int i = 0; i <= n; i++) {
+            r *= Math.Pow(Math.Sin(x), i) + 1/Math.Sqrt(x);
+
+            if (i % 4 == 0) {
+                Console.WriteLine(r);
+            }
+        }
+        Console.WriteLine("Для завершения нажмите любую кнопку");
+        Console.ReadKey();
+    }
+}
+
---
+++ second-semester/Информатика/Решения/ЗО Лабораторные работы/Лабораторная 4/code-number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        uint myNumber = 5;
+        uint k = 14;
+        uint result = myNumber % k;
+        if (result == 0)
+        {
+            result = k;
+        }
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+}

545833f60a925b631f637bda46d806a592ee86e9
Rephobia <26533256+Rephobia@users.noreply.github.com>
2024-06-08 20:08:24 +0300

    Add laba 3 for informatics

---
+++ second-semester/Информатика/Решения/ЗО Лабораторные работы/Лабораторная 3/code/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите порядковый номер карты в колоде");
+
+        int k = Convert.ToInt32(Console.ReadLine());
+
+        if (k < 6 || k > 14) {
+            Console.WriteLine("Ошибка: Карта не существует в колоде");
+        }
+        else {
+            if (k < 11) {
+                Console.WriteLine("Карта: " + k);
+            } else {
+                switch (k) {
+                    case 11: 
+                        Console.WriteLine("Карта: валет");
+                        break;
+                    case 12:
+                        Console.WriteLine("Карта: дама");
+                        break;
+                    case 13:
+                        Console.WriteLine("Карта: король");
+                        break;
+                    case 14:
+                        Console.WriteLine("Карта: туз");
+                        break;
+                }
+            }
+        }
+        Console.WriteLine("Нажмите любую клавишу для завершения программы");
+        Console.ReadKey();
+    }
+}
+

6f5d8731e36e8d5f9aaba20bf35b20b1d007e48e
Rephobia <26533256+Rephobia@users.noreply.github.com>
2024-06-24 16:08:16 +0300

    Add laba 5 for computer science

---
+++ second-semester/Информатика/Решения/ЗО Лабораторные работы/Лабораторная 5/code/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double e = 1E-5;
+        double g = 1E+5;
+        int n = 1;
+        double u;
+        double denominator = (98 + 2 * n);
+        bool isConvergent;
+        bool isNotConvergent;
+        double sum = 0;
+        do
+        {
+            u = Factorial(3 * n - 2) / denominator;
+            denominator *= (98 + 2 * n);
+            sum += u;
+
+            Console.WriteLine(
+                "Номер итерации: " + n + 
+                ", значение члена ряда: " + u + 
+                ", частичная сумма: " + sum
+            );
+
+            n++;
+            isConvergent = u > e;
+            isNotConvergent = u < g;
+        } while (isConvergent && isNotConvergent);
+
+        if (isConvergent)
+        {
+            Console.WriteLine("Ряд предположительно сходится");
+        }
+        if (isNotConvergent)
+        {
+            Console.WriteLine("Ряд не сходится");
+        }
+        Console.ReadKey();
+    }
+
+    static int Factorial(int n)
+    {
+        int result = 1;
+        for (int i = n; i > 0; i--)
+        {
+            result *= i;
+        }
+        return result;
+    }
+}

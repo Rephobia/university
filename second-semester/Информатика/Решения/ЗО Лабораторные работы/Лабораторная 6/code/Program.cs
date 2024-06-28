1693460bddd46908740c0b33277a94719d52ff0c
Rephobia <26533256+Rephobia@users.noreply.github.com>
2024-06-28 12:30:36 +0300

    Add laba 6

--- second-semester/Информатика/Решения/ЗО Лабораторные работы/Лабораторная 6/code/Program.cs
+++ second-semester/Информатика/Решения/ЗО Лабораторные работы/Лабораторная 6/code/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 
-
 internal class Program
 {
     static double Average(int[] data)
@@ -11,12 +10,50 @@ internal class Program
             sum += data[i];
         }
 
-        return sum / data.Length;
+        return (double) sum / (double) data.Length;
     }
 
     static void Main(string[] args)
     {
+        Console.WriteLine("Введите размер массива:");
         int inputSize = Convert.ToInt32(Console.ReadLine());
+        int[] inputData = new int[inputSize];
+
+        Console.WriteLine("Введите элементы массива:");
+
+        for (int i = 0; i < inputSize; i++)
+        {
+            inputData[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        if (inputData.Length > 0)
+        {
+            int max = inputData[0];
+            int maxIndex = 0;
+
+            for (int i = 0; i < inputData.Length; i++)
+            {
+                if (inputData[i] > max)
+                {
+                    max = inputData[i];
+                    maxIndex = i;
+                }
+            }
+
+            int[] result = new int[maxIndex + 1];
+
+            for (int j = 0; j < result.Length; j++)
+            {
+                result[j] = inputData[j];
+            }
+
+            double avgInputData = Average(inputData);
+            double avgResult = Average(result);
+
+            Console.WriteLine("Исходный массив: " + String.Join(", ", inputData) + " Среднее арифметическое: " + avgInputData);
+            Console.WriteLine("Сформированный массив: " + String.Join(", ", result) + " Среднее арифметическое: " + avgResult);
+        }
+        Console.ReadKey();
     }
 }
 

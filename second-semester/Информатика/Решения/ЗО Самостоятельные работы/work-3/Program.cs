69b406c3c9e433d199d7b08025ee23478b0ce2f4
Rephobia <26533256+Rephobia@users.noreply.github.com>
2024-07-02 00:15:05 +0300

    Add cs

---
+++ second-semester/Информатика/Решения/ЗО Самостоятельные работы/work-3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите коэффициент b");
+        double b = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите коэффициент c");
+        double c = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите количество элементов n");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите первый элемент");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        double prevElement = a;
+        for (int i = 0; i < n; i++)
+        {
+            double element = b * prevElement + c;
+            prevElement = element;
+            Console.WriteLine(element + "\t");
+        }
+        Console.ReadKey();
+    }
+}
+
---
+++ second-semester/Информатика/Решения/ЗО Самостоятельные работы/work-4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите a:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите b:");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите c:");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        double firstCheck = Math.Pow(Math.Abs(a), b) - 2;
+        double secondCheck = b * (Math.Pow(c, 3) - 1);
+
+        while (firstCheck < 0 && secondCheck <= 0)
+        {
+            Console.WriteLine("Данные не в области определения функции!");
+            Console.WriteLine("Введите a:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите b:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите c:");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            firstCheck = Math.Pow(Math.Abs(a), b) - 2;
+            secondCheck = b * (Math.Pow(c, 3) - 1);
+        }
+
+        Console.WriteLine("Результат:");
+        Console.WriteLine(Math.Sqrt(Math.Pow(Math.Abs(a), b) - 2) / b * (Math.Pow(c, 3) - 1));
+        Console.ReadKey();
+    }
+}

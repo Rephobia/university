c956e16bb9c746f9b37dea434b7ea8150abe8ae9
Rephobia <26533256+Rephobia@users.noreply.github.com>
2024-07-02 10:32:35 +0300

    Add sr to computer scientce

---
+++ second-semester/Информатика/Решения/ЗО Самостоятельные работы/work-2-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число a");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число b");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число c");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        int result = 0;
+        if (a <= 0)
+        {
+            result++;
+        }
+        if (b <= 0)
+        {
+            result++;
+        }
+        if (c <= 0)
+        {
+            result++;
+        }
+
+        Console.WriteLine("Количество неположительных чисел: " + result);
+        Console.ReadKey();
+    }
+}
---
+++ second-semester/Информатика/Решения/ЗО Самостоятельные работы/work-2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число a");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число b");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число c");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        int result = 0;
+        if (a <= 0 && b <= 0 && c <= 0)
+        {
+            result = 3;
+        } 
+        else if ((a <= 0 && b <= 0) || (a <= 0 && c <= 0) || (b <= 0 && c <= 0))
+        {
+            result = 2;
+        } else if (a <= 0 || b <= 0 || c <= 0) 
+        {
+            result = 1;
+        }
+
+        Console.WriteLine("Количество неположительных чисел: " +  result);
+        Console.ReadKey();
+    }
+}

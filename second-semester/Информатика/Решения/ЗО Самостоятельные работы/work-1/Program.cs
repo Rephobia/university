48fcc47e992ef2d9172240052ceb7aa32b3a61b4
Rephobia <26533256+Rephobia@users.noreply.github.com>
2024-07-02 12:07:41 +0300

    Add sr

---
+++ second-semester/Информатика/Решения/ЗО Самостоятельные работы/work-1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите a:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите b:");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите c:");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        double y = Math.Sqrt(Math.Pow(Math.Abs(a), b) - 2) / b * (Math.Pow(c, 3) - 1);
+        Console.WriteLine("Результат:"  + y);
+        Console.ReadKey();
+    }
+}
+
+

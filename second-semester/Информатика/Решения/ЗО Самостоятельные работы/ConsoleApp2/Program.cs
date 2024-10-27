4ee614ce50ca60c3d6c9a2b339c5e6ae947caf8e
Rephobia <26533256+Rephobia@users.noreply.github.com>
2024-10-27 22:33:32 +0300

    Add cats:

---
+++ second-semester/Информатика/Решения/ЗО Самостоятельные работы/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 3;
+            Console.Write(Sum(5, 3, out c) + "");
+
+        }
+
+        static int Sum(int a, int b, out int c)
+        {
+            c = a + b;
+            return a + b + 10;
+        }
+    }
+}
